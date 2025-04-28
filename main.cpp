42a676bab66fd527a554808dcf21d7a0301e633b
reyeZERO <reyescadenaj28@gmail.com>
2025-04-27 23:23:23 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
-
-#include "statistics.h"
 #include <iostream>
 #include <string>
+#include "statistics.h"
 
 int main() {
     std::cout << "Welcome to the Number Statistics Program!\n" << std::endl;
@@ -10,11 +9,28 @@ int main() {
     std::cout << "Please enter the name of your data file: ";
     std::getline(std::cin, filename);
 
-    while (!processFile(filename)) {
-        std::cout << "\nPlease enter another name: ";
+    int count;
+    double mean, stddev, min, max;
+
+    while (!process_file(filename, count, mean, stddev, min, max)) {
+        std::cout << "I'm sorry, I could not open '" << filename << "' or the file is empty.\n" << std::endl;
+        std::cout << "Please enter another name: ";
         std::getline(std::cin, filename);
     }
 
+    std::cout << "\nFile '" << filename << "' opened successfully!" << std::endl;
+    std::cout << "Reading data from '" << filename << "'..." << std::endl;
+    std::cout << "Calculating..." << std::endl;
+    std::cout << "Done processing data!\n" << std::endl;
+
+    std::cout << "For your data, the statistics are as follows:\n" << std::endl;
+    std::cout << "     Count:  " << count << std::endl;
+    std::cout << "   Minimum:  " << min << std::endl;
+    std::cout << "      Mean:  " << mean << std::endl;
+    std::cout << "   Maximum:  " << max << std::endl;
+    std::cout << "    StdDev:  " << stddev << std::endl;
+
     std::cout << "\nThank you for using the Number Statistics Program!" << std::endl;
+
     return 0;
 }
